f9c2c65f2697a1815c542cdbd8476976b6385fca
eirik124 <eirik.lundanes@gmail.com>
2020-10-15 11:35:22 +0200

    Updated  files with files from previous project all tests should run green

--- src/main/java/no/kristiania/httpServer/HttpServer.java
+++ src/main/java/no/kristiania/httpServer/HttpServer.java
@@ -1,4 +1,155 @@
 package no.kristiania.httpServer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HttpServer {
+
+    private File contentRoot;
+    private List<String> teamMemberNames = new ArrayList<>();
+
+    public HttpServer(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    handleRequest(clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        HttpMessage request = new HttpMessage(clientSocket);
+        String requestLine = request.getStartLine();
+        System.out.println("REQUEST " + requestLine);
+
+        String requestMethod = requestLine.split(" ")[0];
+        String requestTarget = requestLine.split(" ")[1];
+
+        int questionPos = requestTarget.indexOf('?');
+
+        String requestPath = questionPos != -1 ? requestTarget.substring(0, questionPos) : requestTarget;
+
+        if (requestMethod.equals("POST")) {
+            QueryString requestParameter = new QueryString(request.getBody());
+
+            teamMemberNames.add(requestParameter.getParameter("full_name"));
+            String body = "Okay";
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Length: " + body.length() + "\r\n" +
+                    "\r\n" +
+                    body;
+            // Write the response back to the client if post
+            clientSocket.getOutputStream().write(response.getBytes());
+        } else {
+            if (requestPath.equals("/echo")) {
+                handleEchoRequest(clientSocket, requestTarget, questionPos);
+            } else if (requestPath.equals("/api/projectMembers")) {
+                handleGetProducts(clientSocket);
+            } else {
+                File file = new File(contentRoot, requestPath);
+                if (!file.exists()) {
+                    String body = file + " does not exist";
+                    String response = "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: " + body.length() + "\r\n" +
+                            "\r\n" +
+                            body;
+                    // Write the response back to the client if method equals post
+                    clientSocket.getOutputStream().write(response.getBytes());
+                    return;
+                }
+
+
+                String statusCode = "200";
+                //Checking for correct content type and displaying correct type
+                String contentType = "text/plain";
+                if (file.getName().endsWith(".html")) {
+                    contentType = "text/html";
+                } else if (file.getName().endsWith(".css")) {
+                    contentType = "text/css";
+                }
+                String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                        "Content-Length: " + file.length() + "\r\n" +
+                        "Connection: close\r\n" +
+                        "Content-Type: " + contentType + "\r\n" +
+                        "\r\n";
+
+
+                //writing response back to the client
+                clientSocket.getOutputStream().write(response.getBytes());
+
+                new FileInputStream(file).transferTo(clientSocket.getOutputStream());
+            }
+        }
+    }
+
+    private void handleGetProducts(Socket clientSocket) throws IOException {
+        String body = "<ul>";
+        for (String teamMemberNames : teamMemberNames) {
+            body += "<li>" + teamMemberNames + "</li>";
+        }
+        body += "</ul>";
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Connection: close\r\n" +
+                "\r\n" +
+                body;
+
+
+        //Writing response to the server
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    private void handleEchoRequest(Socket clientSocket, String requestTarget, int questionPos) throws IOException {
+        String statusCode = "200";
+        String body = "Hello <strong>World</strong>!";
+        if (questionPos != -1) {
+            QueryString queryString = new QueryString(requestTarget.substring(questionPos + 1));
+            if (queryString.getParameter("status") != null) {
+                statusCode = queryString.getParameter("status");
+            }
+            if (queryString.getParameter("body") != null) {
+                body = queryString.getParameter("body");
+            }
+        }
+        String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(8080);
+        server.setContentRoot(new File("src/main/resources"));
+        System.out.print("Serveren kjører på localhost:8080");
+    }
+
+    //SETTERS
+
+    //Setting path to content files
+    public void setContentRoot(File contentRoot) {
+        this.contentRoot = contentRoot;
+    }
+
+    //GETTERS
+
+
+    //Getting team names
+    public List<String> getTeamNames() {
+        return teamMemberNames;
+    }
 }
